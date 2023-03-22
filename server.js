4bf5859baa4f25400fcdac05b0fde9ee3c54e527
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-03-22 10:07:00 +0900

    sever setting

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res, next) => {
+  res.end("Top Page");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});

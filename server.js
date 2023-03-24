54ea134ffa2ed8eaca0c23e227a14bf2ecdcf59d
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-03-24 10:05:21 +0900

    GETメソッドの作成

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const api = require("./routes/api");
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
+
+app.use("/api", api);
 
 app.get("/", (req, res, next) => {
   res.end("Top Page");

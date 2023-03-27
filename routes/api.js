572621c88aa095b38ed4bcb79c09cb7895ff34a9
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-03-27 10:11:18 +0900

    GETメソッドのリクエストヘッダーにX-lang追加してレスポンスの出しわけ

--- routes/api.js
+++ routes/api.js
@@ -4,10 +4,15 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.setHeader("X-Timestamp", Date.now());
   let message = req.query.message;
+  const lang = req.headers["x-lang"];
 
   if (message === "") {
     res.status(400);
-    message = "messageの値が空です。";
+    if (lang === "en") {
+      message = "message is empty.";
+    } else {
+      message = "messageの値が空です。";
+    }
   }
 
   res.send({ message });

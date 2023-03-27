82bd77a92be0f52381b61958bb8aeb5bc003e520
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-03-27 09:43:06 +0900

    GETメソッドのレスポンスヘッダーにX-Timestampを追加

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
+  res.setHeader("X-Timestamp", Date.now());
   let message = req.query.message;
 
   if (message === "") {

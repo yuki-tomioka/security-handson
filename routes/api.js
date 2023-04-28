92fe08857d5e00471d6f0bc373f0f658ef4f8552
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-04-28 10:09:49 +0900

    許可するオリジンをhttp://localhost:3000に限定

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Headers", "X-Token");
   }

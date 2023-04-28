89dadbe23e910642ddc2713713c00a104727d05c
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-04-28 10:16:29 +0900

    複数のオリジンを許可するために配列形式で指定できるように変更

--- routes/api.js
+++ routes/api.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const allowList = ["http://localhost:3000", "http://site.example:3000"];
+
 router.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  if (req.headers.origin && allowList.includes(req.headers.origin)) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+  }
+
   if (req.method === "OPTIONS") {
     res.header("Access-Control-Allow-Headers", "X-Token");
   }

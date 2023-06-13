9836eb647ae691286d787259dce8ac72e5b9eaf3
Tomioka Yuki <tomioka.yuki@hrbrain.co.jp>
2023-06-13 10:25:19 +0900

    CSP検証ページの準備

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const api = require("./routes/api");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use(express.static("public"));
 
 app.use("/api", api);
 
 app.get("/", (req, res, next) => {
   res.end("Top Page");
+});
+
+app.get("/csp", (req, res) => {
+  res.render("csp");
 });
 
 app.listen(port, () => {
